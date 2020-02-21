e654687170afdf21c984701af15ec333311a39f1
aliyakm <57257098+aliyakm@users.noreply.github.com>
2020-02-21 21:20:59 +0600

    Update and rename b11.py to 1h.py

---
+++ 1h.py
@@ -0,0 +1,6 @@
+a=179**10
+b=str(a)
+b=b*4
+c=int(b)
+d=c**(1/10)
+print(d)
--- b11.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-i = 2
-for i in range(2, n+1):
-    if n%i==0:
-        print(i)
-        exit(0)
-    else:
-        i=i+1
